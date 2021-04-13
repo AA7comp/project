2e0280a98f1eda3182dc27d7416bc5bacf2fd6a1
AA7comp <190103347@stu.sdu.edu.kz>
2021-04-13 23:13:36 +0600

    1
    Start

---
+++ routes/web.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UploadFileController;
+use App\Http\Controllers\AccountInfoController;
+use App\Http\Controllers\PostController;
+
+
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/posts/index', [PostController::class, 'index'])->name('post.index');
+Route::get('/post/create', [PostController::class, 'create'])->name('post.create');
+Route::post('/post/store', [PostController::class, 'store'])->name('post.store');
+
+Route::get('/upload', [UploadFileController::class, 'index'])->name('upload-index');
+Route::post('/upload', [UploadFileController::class, 'store'])->name('upload-store');
+Route::get('/accounts',[AccountInfoController::class, 'index'])->name('accounts');
+Route::get('/send-email/{id}', [MailController::class, 'sendEmail'])->name('send-email');
